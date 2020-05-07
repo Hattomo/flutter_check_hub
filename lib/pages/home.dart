20ffe62f4c637143ce6f08c5839e25a639ec0ebb
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-05-07 23:13:54 +0900

    Add showBottomModalSheel to create item

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_check_hub/models/user.dart';
 import 'package:flutter_check_hub/pages/graph/graph_home.dart';
 import 'package:flutter_check_hub/pages/recordhome/record_home.dart';
 import 'package:flutter_check_hub/pages/setting/settings.dart';
 import 'package:flutter_check_hub/service/date_time.dart';
+import 'package:flutter_check_hub/service/user_dataservice.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CheckHubHome extends StatefulWidget {
@@ -101,75 +104,80 @@ class _CheckHubHomeState extends State<CheckHubHome>
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
+    final User user = Provider.of<User>(context);
+    print(user.uid);
+    return StreamProvider.value(
+      value: DatabaseServiceUser().user(user),
+      child: DefaultTabController(
+        length: 3,
+        child: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                      icon: const Icon(Icons.arrow_back_ios),
+                      onPressed: () {
+                        dateTimeManager.subtractDate();
+                      }),
+                  StreamBuilder<DateTime>(
+                      stream: dateTimeManager.dateStream,
+                      initialData: dateTimeManager.selectedDate,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<DateTime> snapshot) {
+                        return InkWell(
+                          onTap: () => showCalendar(),
+                          child: Column(
+                            children: <Widget>[
+                              Text(DateFormat('EEEE')
+                                  .format(dateTimeManager.selectedDate)),
+                              Text(DateFormat('MMM d y')
+                                  .format(dateTimeManager.selectedDate)),
+                            ],
+                          ),
+                        );
+                      }),
+                  IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios),
+                      onPressed: () {
+                        dateTimeManager.addDate();
+                      }),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              controller: _tabController,
               children: <Widget>[
-                IconButton(
-                    icon: const Icon(Icons.arrow_back_ios),
-                    onPressed: () {
-                      dateTimeManager.subtractDate();
-                    }),
-                StreamBuilder<DateTime>(
-                    stream: dateTimeManager.dateStream,
-                    initialData: dateTimeManager.selectedDate,
-                    builder: (BuildContext context,
-                        AsyncSnapshot<DateTime> snapshot) {
-                      return InkWell(
-                        onTap: () => showCalendar(),
-                        child: Column(
-                          children: <Widget>[
-                            Text(DateFormat('EEEE')
-                                .format(dateTimeManager.selectedDate)),
-                            Text(DateFormat('MMM d y')
-                                .format(dateTimeManager.selectedDate)),
-                          ],
-                        ),
-                      );
-                    }),
-                IconButton(
-                    icon: const Icon(Icons.arrow_forward_ios),
-                    onPressed: () {
-                      dateTimeManager.addDate();
-                    }),
+                RecordHome(),
+                GraphHome(),
+                Settings(),
               ],
             ),
-          ),
-          body: TabBarView(
-            controller: _tabController,
-            children: <Widget>[
-              RecordHome(),
-              GraphHome(),
-              Settings(),
-            ],
-          ),
-          bottomNavigationBar: SafeArea(
-            child: TabBar(
-              controller: _tabController,
-              indicatorWeight: 2.0,
-              labelColor: Colors.blue,
-              unselectedLabelColor: Colors.grey,
-              unselectedLabelStyle: const TextStyle(fontSize: 12.0),
-              tabs: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(Icons.layers),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(Icons.insert_chart),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(Icons.settings),
-                ),
-              ],
+            bottomNavigationBar: SafeArea(
+              child: TabBar(
+                controller: _tabController,
+                indicatorWeight: 2.0,
+                labelColor: Colors.blue,
+                unselectedLabelColor: Colors.grey,
+                unselectedLabelStyle: const TextStyle(fontSize: 12.0),
+                tabs: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.layers),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.insert_chart),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.settings),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/pages/setting/edit_item_home.dart
+++ lib/pages/setting/edit_item_home.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_check_hub/models/user.dart';
 import 'package:flutter_check_hub/service/data_store_service.dart';
 import 'package:flutter_check_hub/shared/text_input_decoration.dart';
-import 'package:provider/provider.dart';
 
 class EditItemHome extends StatefulWidget {
+  EditItemHome({Key key, this.user}) : super(key: key);
+  User user;
   @override
   _EditItemHomeState createState() => _EditItemHomeState();
 }
@@ -15,102 +16,139 @@ class _EditItemHomeState extends State<EditItemHome> {
   String currentgoal;
   String currenticon;
 
-  UserData user;
   final DatabaseServiceItem dataServiceItem = DatabaseServiceItem();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    final UserData user = Provider.of<UserData>(context) ??
-        UserData(uid: '', name: '', itemsid: ['']);
-    print(user);
-    print(user.uid);
-    print(user.name);
-    print(user.itemsid);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Item'),
-        leading: FlatButton(
-          child: const Icon(Icons.cancel),
-          onPressed: () => Navigator.pop(context),
-        ),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('Done'),
-            onPressed: () async {
+    print(widget.user);
+    print(widget.user.uid);
+    print(widget.user.name);
+    print(widget.user.itemsid);
+    return Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(25.0),
+                          topRight: Radius.circular(25.0))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.cancel,
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                const Text('Create Item'),
+                IconButton(
+                  icon: const Icon(Icons.done),
+                  onPressed: () async {
               await dataServiceItem.createItemData(
-                  uid: user.uid, title: 'walk', data: 'yy');
+                  uid: widget.user.uid, title: 'walk', data: 'yy');
             },
+                )
+              ],
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Title 🚀'),
+                      validator: (String val) =>
+                          val.isEmpty ? 'Please enter a title' : null,
+                      onChanged: (String val) =>
+                          setState(() => currenttitle = val),
+                    ),
+                    leading: const Icon(Icons.title),
+                  ),
+                ),
+                const SizedBox(height: 20.0,),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: const Text('Data Type'),
+                    leading: const Icon(Icons.style),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    subtitle: const Text('current data type'),
+                    onTap: () => Navigator.pushNamed(context, '/dataTypeSetting'),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                      leading: const Icon(Icons.ac_unit),
+                      title: TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Unit 🐣'),
+                        validator: (String val) =>
+                            val.isEmpty ? 'Please enter a title' : null,
+                        onChanged: (String val) =>
+                            setState(() => currentunit = val),
+                      )),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: const Icon(Icons.insert_emoticon),
+                    title: TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                          hintText: 'Please input one emoji 😀'),
+                      validator: (String val) =>
+                          val.isEmpty ? 'Please enter a title' : null,
+                      onChanged: (String val) =>
+                          setState(() => currenticon = val),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: const Icon(Icons.repeat),
+                    title: const Text('Repeat'),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    onTap: () => Navigator.pushNamed(context, '/repeatSetting'),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: const Icon(Icons.notifications),
+                    title: const Text('Notification'),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    onTap: () {},
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: const Icon(Icons.iso),
+                    title: TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                          hintText: 'Please input goal 🎉'),
+                      validator: (String val) =>
+                          val.isEmpty ? 'Please enter a title' : null,
+                      onChanged: (String val) =>
+                          setState(() => currentgoal = val),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
-      ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(
-              height: 20.0,
-            ),
-            ListTile(
-              title: TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Title 🚀'),
-                validator: (String val) =>
-                    val.isEmpty ? 'Please enter a title' : null,
-                onChanged: (String val) => setState(() => currenttitle = val),
-              ),
-              leading: const Icon(Icons.title),
-            ),
-            ListTile(
-              title: const Text('Data Type'),
-              leading: const Icon(Icons.style),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              subtitle: const Text('current data type'),
-              onTap: () => Navigator.pushNamed(context, '/dataTypeSetting'),
-            ),
-            ListTile(
-                leading: const Icon(Icons.ac_unit),
-                title: TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Unit 🐣'),
-                  validator: (String val) =>
-                      val.isEmpty ? 'Please enter a title' : null,
-                  onChanged: (String val) => setState(() => currentunit = val),
-                )),
-            ListTile(
-              leading: const Icon(Icons.insert_emoticon),
-              title: TextFormField(
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Please input one emoji 😀'),
-                validator: (String val) =>
-                    val.isEmpty ? 'Please enter a title' : null,
-                onChanged: (String val) => setState(() => currenticon = val),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.repeat),
-              title: const Text('Repeat'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              onTap: () => Navigator.pushNamed(context, '/repeatSetting'),
-            ),
-            ListTile(
-              leading: const Icon(Icons.notifications),
-              title: const Text('Notification'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.iso),
-              title: TextFormField(
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Please input goal 🎉'),
-                validator: (String val) =>
-                    val.isEmpty ? 'Please enter a title' : null,
-                onChanged: (String val) => setState(() => currentgoal = val),
-                onTap: () {},
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
